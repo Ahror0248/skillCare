c6c2573d978c40da900af13f580e8f7041d21450
ilosrim <mirsolikhdev@gmail.com>
2021-09-11 14:25:22 -0400

    Build Footer sections

---
+++ src/root/root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer } from '../Components/Footer'
+import { SubFooter } from '../Components/SubFooter'
+import { JoinWithUs } from '../Components/JoinWithUs'
+
+export const Root = () => {
+  return (
+    <>
+      <JoinWithUs />
+      <Footer />
+      <SubFooter />
+    </>
+  )
+}
