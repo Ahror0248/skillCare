de3b0be9340da67d5004cd6e1d18c2e3015a3552
Ahror0248 <akhrorcodrr@gmail.com>
2021-09-18 12:51:33 +0500

    HeaderNavbar

---
+++ src/Components/navbar/style.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const All = styled.div`
+  background-color: #f8fcfe;
+`;
+
+export const NavWrapper = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  padding: 75px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.img`
+  cursor: pointer;
+  margin-right: 80px;
+`;
+
+export const Ul = styled.ul`
+  list-style: none;
+`;
+
+export const Li = styled.li`
+  font-size: 20px;
+  line-height: 27px;
+  color: #666571;
+  font-weight: 500;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+  :hover {
+    font-weight: 500;
+    color: #26242e;
+    transform: scale3d(1.1, 1.1, 1.1);
+    font-family: "Open Sans", sans-serif;
+  }
+`;
+
+export const NavButton = styled.button`
+  width: ${({headerbtn})=> headerbtn? '207px':'156px'} ;
+  height: 57px;
+  font-family: ${({headerbtn})=>headerbtn? 'Montserrat Alternates, sans-serif;' : '"Montserrat", sans-serif'};
+  background: #6347eb;
+  border-radius: 12px;
+  outline: none;
+  border: none;
+  font-size: 20px;
+  line-height: 24px;
+  text-align: center;
+  color: #ffffff;
+  font-weight: 600;
+  transition: 0.3s linear;
+  :hover {
+    background: #7f6ee0;
+  }
+  :active {
+    transform: scale(0.8);
+  }
+`;
---
+++ src/root/root.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../Components/header'
+import { Navbar } from '../Components/navbar'
+
+export const Root = () => {
+    return (
+        <div>
+            <Navbar />
+            <Header />
+        </div>
+    )
+}
